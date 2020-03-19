1d5bc76eb54ab355900e81cad0f44f5b2213c7ea
timwh <62370120+timwh@users.noreply.github.com>
2020-03-19 15:30:09 +0930

    Create gpsd_logger.py

---
+++ gpsd_logger.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python3
+# -------------------------------------------------------------------
+# A data logger GPS on RPi zero W
+# GPS connected to ttyS0 (serial0)
+# Tim Whiteside
+# For it to work:
+#  - make sure all relevant libraries/packages are installed
+#  - place this script in /home/pi
+#  - create a folder in /home/pi called data
+# -------------------------------------------------------------------
+# LIBRARIES
+# requires gpsd service daemon installed and running--- https://gpsd.gitlab.io/gpsd/index.html
+# also requires the package gps3 installed as an interface to gpsd --- https://pypi.org/project/gps3/
+import gpsd
+from time import strftime, sleep
+
+# GLOBAL
+timestr=strftime("%Y%m%d--%H%M%S-%Z")
+
+# FUNCTIONS
+# Log file setup
+def get_file_name():
+    #use date and time to name file
+    return "GPSlog_" + timestr + ".txt"
+# Get full path for writing and set up headers
+name = get_file_name()
+path = "/home/pi/data/"+ name
+with open(path,"w") as log:
+    log.write("time,lat,long\n")
+
+# OPEN PORTS
+# Connect to the local gpsd
+gpsd.connect()
+
+# Get current gps position
+packet = gpsd.get_current()
+
+#Run loop
+while True:
+    packet = gpsd.get_current()  # poll GPS location
+    if packet.mode >= 2:
+        print("Time: " + str(packet.time), " Latitude: " + str(packet.lat), " Longitude: " + str(packet.lon), "  Mode: " + str(packet.mode),"Sats: " + str(packet.sats))
+        # write to file
+        with open(path, "a") as log:
+            log.write("{0},{1},{2}\n".format(str(packet.time), str(packet.lat), str(packet.lon)))
+            sleep(1)
+
+    else:
+        print(" Latitude: NA")
+        print(" Longitude: NA")
+        
+
+
+
