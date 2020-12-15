041f84bdcc1f56c3bb9e32eeb103b0b96f947fc4
Tim Whiteside <62370120+timwh@users.noreply.github.com>
2020-12-15 15:50:33 +0930

    Update gpsd_logger.py

--- gpsd_logger.py
+++ gpsd_logger.py
@@ -9,17 +9,17 @@
 #  - create a folder in /home/pi called data
 #  - requires gpsd service daemon installed and running--- https://gpsd.gitlab.io/gpsd/index.html
 #  - also requires the package gpsd-py3 installed as an interface to gpsd --- https://github.com/MartijnBraam/gpsd-py3
-#  - if the system command in the script below does not work, prior to re-running the script you can start the gpsd daemon with this command: sudo gpsd /dev/serial0 -F /var/run/gpsd.sock
+#  - if the system(startCmd) command in the script below does not work, prior to re-running the script you can start the gpsd daemon with this command: sudo gpsd /dev/serial0 -F /var/run/gpsd.sock
 # -------------------------------------------------------------------
 
 # LIBRARIES
 import gpsd
 from time import strftime, sleep
-# from os import system  # needed to run start daemon (pi zero doesn't start it automatically for some reason)
+from os import system  # needed to run start daemon (pi zero doesn't start it automatically for some reason)
 
 # starting gpsd daemon
-# startCmd = 'sudo gpsd /dev/serial0 -F /var/run/gpsd.sock'
-# system(startCmd)
+startCmd = 'sudo gpsd /dev/serial0 -F /var/run/gpsd.sock'
+system(startCmd)
 
 # GLOBAL
 timestr=strftime("%Y%m%d--%H%M%S-%Z")
